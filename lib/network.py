548673e6117fcaa2fbaa6b8c53472bcc11cae454
Phan Huy Hoang <hoangph271@gmail.com>
2022-11-08 00:10:47 +0700

    [Add] network.connect_wifi

--- lib/network.py
+++ lib/network.py
@@ -1,2 +1,17 @@
-def connect_wifi():
-    print(1)
+import network, rp2, utime
+
+def connect_wifi(country, ssid, password):
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    rp2.country(country)
+
+    # set power mode to get WiFi power-saving off (if needed)
+    wlan.config(pm = 0xa11140)
+    wlan.connect(ssid, password)
+
+    while not wlan.isconnected() and wlan.status() >= 0:
+        print("-- Waiting to connect:")
+        utime.sleep(1.25)
+
+    print(f'-- Connected to {ssid}, IPs:')
+    print(wlan.ifconfig())
--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-from machine import Pin
-import utime
+from config import WIFI_COUNTRY, WIFI_SSID, WIFI_PASS
 
-import lib.config as config
 import lib.network as network
 
-led = Pin('LED', Pin.OUT)
-
-led.on()
-
-utime.sleep(1.25)
-
-led.off()
-
-network.connect_wifi()
-
-print('exit(0)')
+network.connect_wifi(WIFI_COUNTRY, WIFI_SSID, WIFI_PASS)
