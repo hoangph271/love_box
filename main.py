4299231135bc3e75e26cef5ca0590b7f8682256e
Phan Huy Hoang <hoangph271@gmail.com>
2022-11-09 04:57:12 +0700

    [Fix] Deadcode

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ from config import WIFI_COUNTRY, WIFI_SSID, WIFI_PASS
 import lib.wifi as wifi
 import lib.http_server as http_server
 
-# import lib.led as led
-# from lib.http_helper import send_ok, send_404, send_text
-
 wifi.config(WIFI_COUNTRY)
 
 #region __main__
