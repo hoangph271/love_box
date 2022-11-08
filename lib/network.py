5713d5191e396e54b5274222c13f2d73b9b73194
Phan Huy Hoang <hoangph271@gmail.com>
2022-11-08 14:15:17 +0700

    [Fix] Filter IP when connect_wifi()

--- lib/network.py
+++ lib/network.py
@@ -6,14 +6,14 @@ def connect_wifi(country, ssid, password):
     rp2.country(country)
 
     # set power mode to get WiFi power-saving off (if needed)
-    wlan.config(pm = 0xa11140)  # type: ignore
+    wlan.config(pm = 0xa11140) # type: ignore
     wlan.connect(ssid, password)
 
     while not wlan.isconnected() and wlan.status() >= 0:
         print('-- Waiting to connect:')
         utime.sleep(1.25)
 
-    return wlan.ifconfig()[0]
+    return next(ip for ip in wlan.ifconfig() if ip.startswith('192.168'))
 
 def open_socket(ip):
     address = (ip, 80)
