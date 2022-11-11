10e038625f84f3ac62a22c1ac26065c8f7c8d744
Phan Huy Hoang <hoangph271@gmail.com>
2022-11-11 19:33:25 +0700

    [Add] Scaffold kv_db

--- main.py
+++ main.py
@@ -10,10 +10,14 @@ wifi.config(WIFI_COUNTRY)
 #region __main__
 print(f'-- STARTED: CPU@{freq() / 1_000_000}MHz...!')
 
-ap_ip = wifi.host_wifi(AP_SSID, AP_PASS)
+from lib.kv_db import init_db, get_json
+init_db(KV_DB_DIR)
+get_json('aps')
 
-server_ip = wifi.join_wifi(WIFI_SSID, WIFI_PASS)
-start_server(server_ip)
+# ap_ip = wifi.host_wifi(AP_SSID, AP_PASS)
+
+# server_ip = wifi.join_wifi(WIFI_SSID, WIFI_PASS)
+# start_server(server_ip)
 
 print('-- TERMINATED...!')
 #endregion
