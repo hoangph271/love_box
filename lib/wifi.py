f999ed19140fbb70e5ce530cfb83d0ab26a4890e
Phan Huy Hoang <hoangph271@gmail.com>
2022-11-11 16:06:16 +0700

    [Fix] Use single wlan instance

--- lib/wifi.py
+++ lib/wifi.py
@@ -1,5 +1,10 @@
 import network, rp2, utime, socket
 
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+# set power mode to get WiFi power-saving off (if needed)
+wlan.config(pm = 0xa11140) # type: ignore
+
 def config(country):
     rp2.country(country)
 
@@ -8,6 +13,9 @@ def _filter_ips(ips):
     print(f'> IPs: {formatted_ips}')
 
     return next(ip for ip in ips if ip.startswith('192.168.'))
+
+def scan_wifi():
+    return wlan.scan()
 
 def host_wifi(ssid, password):
     ap = network.WLAN(network.AP_IF)
@@ -23,15 +31,10 @@ def host_wifi(ssid, password):
     return _filter_ips(ap.ifconfig())
 
 def join_wifi(ssid, password):
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
-
-    # set power mode to get WiFi power-saving off (if needed)
-    wlan.config(pm = 0xa11140) # type: ignore
     wlan.connect(ssid, password)
 
     while not wlan.isconnected() and wlan.status() >= 0:
-        print(f'> Connecting AP [{ssid}]...!')
+        print(f'> Joining AP [{ssid}]...!')
         utime.sleep(1.25)
 
     print(f'> Joined AP: {ssid}...!')
