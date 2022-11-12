8e02eedf3c62d7ef79d0ff9262fb1805e323bb41
Phan Huy Hoang <hoangph271@gmail.com>
2022-11-13 04:34:33 +0700

    [Fix] Revert main.py changes

--- main.py
+++ main.py
@@ -10,14 +10,10 @@ wifi.config(WIFI_COUNTRY)
 #region __main__
 print(f'-- STARTED: CPU@{freq() / 1_000_000}MHz...!')
 
-from lib.kv_db import init_db, get_json
-init_db(KV_DB_DIR)
-get_json('aps')
+ap_ip = wifi.host_wifi(AP_SSID, AP_PASS)
 
-# ap_ip = wifi.host_wifi(AP_SSID, AP_PASS)
-
-# server_ip = wifi.join_wifi(WIFI_SSID, WIFI_PASS)
-# start_server(server_ip)
+server_ip = wifi.join_wifi(WIFI_SSID, WIFI_PASS)
+start_server(server_ip)
 
 print('-- TERMINATED...!')
 #endregion
