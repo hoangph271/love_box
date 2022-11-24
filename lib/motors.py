b2d30b738cba5e630b7ad899a014f83f2e80e6e3
Phan Huy Hoang <hoangph271@gmail.com>
2022-11-24 11:16:24 +0700

    [Add] lib/motors.py functions

--- lib/motors.py
+++ lib/motors.py
@@ -1,11 +1,30 @@
 from machine import Pin
-import uasyncio
 
-async def setup_motors():
-    IN1 = Pin(2, Pin.OUT)
-    IN2 = Pin(3, Pin.OUT)
+IN1 = Pin(6, Pin.OUT)
+IN2 = Pin(7, Pin.OUT)
+IN3 = Pin(4, Pin.OUT)
+IN4 = Pin(3, Pin.OUT)
 
+def right_forward():
+    IN1.high()
+    IN2.low()
+def right_backward():
     IN1.low()
     IN2.high()
+def stop_right():
+    IN1.low()
+    IN2.low()
 
-    await uasyncio.sleep(1.25)
+def left_forward():
+    IN3.high()
+    IN4.low()
+def left_backward():
+    IN3.low()
+    IN4.high()
+def stop_left():
+    IN3.low()
+    IN4.low()
+
+def stop_all():
+    stop_right()
+    stop_left()
--- main.py
+++ main.py
@@ -2,20 +2,22 @@ import sys, machine, uasyncio
 
 from config import *
 from lib.http_server.http_server import start_server
-from lib.motors import setup_motors
+from lib.led import led_on, led_off
 import lib.wifi as wifi
 
 async def main():
     print(f'-- STARTED: CPU@{machine.freq() / 1_000_000}MHz...!')
+    led_on()
+
+    await uasyncio.sleep(1)
 
     await wifi.host_wifi(AP_SSID, AP_PASS)
     server_ip = await wifi.join_wifi(WIFI_SSID, WIFI_PASS)
 
     start_server(server_ip)
-    await setup_motors()
 
     print('-- TERMINATING...!')
+    led_off()
     sys.exit(0)
 
-uasyncio.run(setup_motors())
 uasyncio.run(main())
